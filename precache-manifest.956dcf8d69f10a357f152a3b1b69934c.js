e1053da0593500f1896bad09871401c2e3742426
rajkumar-101 <rajmummies@gmail.com>
2020-08-18 15:49:12 +0530

    Updates

---
+++ precache-manifest.956dcf8d69f10a357f152a3b1b69934c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c16cd3a88224ddbcd0ae3edd1edffb",
+    "url": "/lyric-searcher-app/index.html"
+  },
+  {
+    "revision": "db6eb506a84d24475359",
+    "url": "/lyric-searcher-app/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "22b28408a95f2e968539",
+    "url": "/lyric-searcher-app/static/js/2.31a9a660.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/lyric-searcher-app/static/js/2.31a9a660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db6eb506a84d24475359",
+    "url": "/lyric-searcher-app/static/js/main.2ea9cdeb.chunk.js"
+  },
+  {
+    "revision": "75a6b064555777495a6e",
+    "url": "/lyric-searcher-app/static/js/runtime-main.01700108.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/lyric-searcher-app/static/media/spinner.26ef8bb4.gif"
+  }
+]);
